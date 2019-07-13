e7bb85dff9294331ac438d724c469779b04dbd7a
leoclementliao <meng.liao@qq.com>
2019-07-13 23:19:04 +0200

    + table detection using luminoth

--- app_table_v1.py
+++ app_table_v1.py
@@ -1,5 +1,5 @@
 import datetime
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -21,6 +21,7 @@ import os
 import cv2
 from luminoth import Detector, read_image, vis_objects
 from TesseractOCR import show_ocr
+import json
 
 
 def fig_to_uri(in_fig, close_all=True, **save_args):
@@ -39,9 +40,41 @@ def fig_to_uri(in_fig, close_all=True, **save_args):
     encoded = base64.b64encode(out_img.read()).decode("ascii").replace("\n", "")
     return "data:image/png;base64,{}".format(encoded)
 
-# Creat checkpoint
-#detector = Detector(checkpoint='b4193ef66713')
 
+def array_to_uri(img_array):
+    # TODO convert directly np.array to uri string
+    # type: (np.array) -> str
+    """
+    Save an image array as a URI
+    """
+    if len(img_array.shape) > 3:
+        img_array = img_array[:, :, :3]
+
+    im = Image.fromarray(img_array)
+    im.save('buf.jpg')
+    image = open('buf.jpg', 'rb')
+    image_read = image.read()
+    image64encoding = base64.b64encode(image_read).decode("ascii").replace("\n", "")
+    image_uri = f"data:image/png;base64,{image64encoding}"
+    return image_uri
+
+
+def uri_to_array(content):
+    # type: str -> (np.array)
+    """
+    Save a URI as an image array
+    """
+    msg = base64.b64decode(content.split(',')[1])
+    buf = BytesIO(msg)
+    img_array = np.ascontiguousarray(Image.open(buf))
+
+    if len(img_array.shape) < 3:
+        img_array = np.repeat(img_array[:, :, np.newaxis], 3, axis=2).astype(np.uint8) * 255
+    return img_array
+
+
+# Creat checkpoint
+detector = Detector(checkpoint='6398676e0ced')
 
 
 external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css']
@@ -79,23 +112,28 @@ app.layout = html.Div([
             dcc.Loading(id="loading-1", children=[
                 html.Div(id='output-image-upload', style={'width': '100%'})
             ], type='circle'),
-        ], className="col-6"),
+        ], className="col-4"),
 
         html.Div([
             dcc.Loading(id="loading-2", children=[
+                html.Div(id='output-image-table-windows', style={'width': '100%'})
+            ], type='circle'),
+        ], className="col-4"),
+
+        html.Div([
+            dcc.Loading(id="loading-3", children=[
                 html.Div(id='ocr-conversion-output', style={'width': '90%'})
             ], type='circle'),
-            dcc.Loading(id ="loading-3", children=[
+            dcc.Loading(id="loading-4", children=[
                 html.Div(id='ocr-export-output', style={'width': '100%'})
-            ],type="circle"),
+            ], type="circle"),
             # html.Button(id='export-pdf-button', n_clicks=0, children='Export PDF'),
             # html.Button(id='export-html-button', n_clicks=0, children='Export HTML')
-        ], className="col-6"),
+        ], className="col-4"),
 
     ], className="row"),
 
-
-    ])
+])
 
 
 def parse_contents(contents, filename, date):
@@ -115,6 +153,40 @@ def parse_contents(contents, filename, date):
     ])
 
 
+def table_windows_contents(content):
+    img_array = uri_to_array(content)
+
+
+    img = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+    b = cv2.distanceTransform(img, distanceType=cv2.DIST_L2, maskSize=5)
+    g = cv2.distanceTransform(img, distanceType=cv2.DIST_L1, maskSize=5)
+    r = cv2.distanceTransform(img, distanceType=cv2.DIST_C, maskSize=5)
+
+    # merge the transformed channels back to an image to predict
+    image_map = cv2.merge((b, g, r))
+    objects = detector.predict(image_map)
+    objects_json = json.dumps(objects)
+    img_windows = vis_objects(img_array, objects, scale=2, fill=100)
+    img_windows_array = np.array(img_windows)
+
+    img_uri = array_to_uri(img_windows_array)
+
+    return html.Div([
+        html.H5('Tables detection'),
+        # html.H6(datetime.datetime.fromtimestamp(date)),
+
+        # HTML images accept base64 encoded strings in the same format
+        # that is supplied by the upload
+        html.Img(src=img_uri, style={'width': '100%'}),
+        html.Hr(),
+        html.Div('List of table windows'),
+        html.Pre(objects_json, style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+
 @app.callback(Output('output-image-upload', 'children'),
               [Input('upload-image', 'contents')],
               [State('upload-image', 'filename'),
@@ -125,6 +197,22 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
             parse_contents(c, n, d) for c, n, d in
             zip(list_of_contents, list_of_names, list_of_dates)]
         return children
+
+
+@app.callback(Output('output-image-table-windows', 'children'),
+              [Input('submit-button', 'n_clicks')],
+              [State('upload-image', 'contents')])
+def update_table_windows(n_clicks, list_of_contents):
+    try:
+
+        children = [
+            table_windows_contents(c) for c in list_of_contents
+        ]
+
+    except:
+        children = "Please upload a file"
+
+    return children
 
 
 @app.callback(Output('ocr-conversion-output', 'children'),
@@ -146,28 +234,22 @@ def ocr_conversion(n_clicks, contents):
               [State('upload-image', 'contents')]
               )
 def ocr_export(n_clicks, contents):
-    # pdf_or_html = 'html'
-    # pdf_or_html_output = pytesseract.image_to_pdf_or_hocr(Image.open(filename[0]), extension=pdf_or_html)
-    # filepath = "image/ocr_output." + pdf_or_html
-    # f = open(filepath, "w+b")
-    # f.write(bytearray(pdf_or_html_output))
-    # return html.A('Export PDF', id='exportPDF', className='btn btn-primary', href="image/ocr_output.html", target="_blank", download="rawdata.pdf",)
     try:
         msg = base64.b64decode(contents[0].split(',')[1])
         buf = io.BytesIO(msg)
         export_btns = []
-        for pdf_or_html in ['pdf','hocr']:
+        for pdf_or_html in ['pdf', 'hocr']:
             pdf_or_html_output = pytesseract.image_to_pdf_or_hocr(Image.open(buf), extension=pdf_or_html)
-            if pdf_or_html=='pdf':
+            if pdf_or_html == 'pdf':
                 btnname = 'Export PDF'
                 fn = "ocr_output.pdf"
-                filepath = "static/"+ fn
+                filepath = "static/" + fn
                 f = open(filepath, "w+b")
                 f.write(bytearray(pdf_or_html_output))
             else:
                 btnname = 'Export HTML'
                 fn = "ocr_output.html"
-                filepath = "static/" +fn
+                filepath = "static/" + fn
                 f = open(filepath, "w+b")
                 f.write(pdf_or_html_output)
             f.close()
@@ -179,8 +261,7 @@ def ocr_export(n_clicks, contents):
         export_btns = html.A('')
     return export_btns
 
-    #return html.A('Export PDF', href="static/ocr_output.pdf", target="_blank", download="rawdata.pdf")#('n_clicks {}'.format(n_clicks))
-
+    # return html.A('Export PDF', href="static/ocr_output.pdf", target="_blank", download="rawdata.pdf")#('n_clicks {}'.format(n_clicks))
 
     # export_buttons = []
     # try:
@@ -196,10 +277,12 @@ def ocr_export(n_clicks, contents):
     #     export_buttons = []
     #     pass
 
+
 @app.server.route('/static/<path:path>')
 def static_file(path):
     static_folder = os.path.join(os.getcwd(), 'static')
     return send_from_directory(static_folder, path)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
